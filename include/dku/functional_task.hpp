4d4c3670d2947cf5defa113f0289bafc9f7d7949
Tlhey <110578260+Leah-Tlhey@users.noreply.github.com>
2023-03-15 15:50:48 +0800

    Update functional_task.hpp
    3.15 change port 4 <--> 5

--- include/dku/functional_task.hpp
+++ include/dku/functional_task.hpp
@@ -35,7 +35,7 @@
 #define FLYWHEEL_MOTOR_2_PORT           21
 #define INDEX_MOTOR_PORT                1
 #define ROLLER_MOTOR_PORT               3
-#define EXTENSION_MOTOR_PORT            4
+#define EXTENSION_MOTOR_PORT            5
 
 #define GAS_GPIO_PORT                   1 // port A
 #define EXTENSION_GPIO_PORT             2 // port B
